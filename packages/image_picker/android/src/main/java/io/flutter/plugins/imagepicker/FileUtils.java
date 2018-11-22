f62d537d304360506ccf4b56bbbec2efaae64c96
Joydeep <joydeepsaha05@gmail.com>
2018-11-22 11:49:24 +0530

    - Copying images from Google photos to cache

--- packages/image_picker/android/src/main/java/io/flutter/plugins/imagepicker/FileUtils.java
+++ packages/image_picker/android/src/main/java/io/flutter/plugins/imagepicker/FileUtils.java
@@ -28,7 +28,11 @@ import android.os.Build;
 import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
-import android.text.TextUtils;
+import android.provider.OpenableColumns;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -37,153 +41,292 @@ import java.io.OutputStream;
 
 class FileUtils {
 
-  String getPathFromUri(final Context context, final Uri uri) {
-    String path = getPathFromLocalUri(context, uri);
-    if (path == null) {
-      path = getPathFromRemoteUri(context, uri);
-    }
-    return path;
-  }
+    private static final String DOCUMENTS_DIR = "documents";
 
-  @SuppressLint("NewApi")
-  private String getPathFromLocalUri(final Context context, final Uri uri) {
-    final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    private static String getDataColumn(
+            Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
 
-    if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-      if (isExternalStorageDocument(uri)) {
-        final String docId = DocumentsContract.getDocumentId(uri);
-        final String[] split = docId.split(":");
-        final String type = split[0];
+        final String column = "_data";
+        final String[] projection = {column};
 
-        if ("primary".equalsIgnoreCase(type)) {
-          return Environment.getExternalStorageDirectory() + "/" + split[1];
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
         }
-      } else if (isDownloadsDocument(uri)) { // https://github.com/flutter/flutter/issues/21863#issuecomment-435544682
-        final String id = DocumentsContract.getDocumentId(uri);
+        return null;
+    }
 
-        if (!TextUtils.isEmpty(id)) {
-          if (id.startsWith("raw:")) {
-            return id.replaceFirst("raw:", "");
-          }
-          try {
-            final Uri contentUri =
-                    ContentUris.withAppendedId(
-                            Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-            return getDataColumn(context, contentUri, null, null);
-          } catch (Exception e) {
-            // Do nothing, try something else
-          }
+    private static String getPathFromRemoteUri(final Context context, final Uri uri) {
+        // The code below is why Java now has try-with-resources and the Files utility.
+        File file = null;
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        boolean success = false;
+        try {
+            inputStream = context.getContentResolver().openInputStream(uri);
+            file = File.createTempFile("image_picker", "jpg", context.getCacheDir());
+            outputStream = new FileOutputStream(file);
+            if (inputStream != null) {
+                copy(inputStream, outputStream);
+                success = true;
+            }
+        } catch (IOException ignored) {
+        } finally {
+            try {
+                if (inputStream != null) inputStream.close();
+            } catch (IOException ignored) {
+            }
+            try {
+                if (outputStream != null) outputStream.close();
+            } catch (IOException ignored) {
+                // If closing the output stream fails, we cannot be sure that the
+                // target file was written in full. Flushing the stream merely moves
+                // the bytes into the OS, not necessarily to the file.
+                success = false;
+            }
+        }
+        return success ? file.getPath() : null;
+    }
+
+    private static void copy(InputStream in, OutputStream out) throws IOException {
+        final byte[] buffer = new byte[4 * 1024];
+        int bytesRead;
+        while ((bytesRead = in.read(buffer)) != -1) {
+            out.write(buffer, 0, bytesRead);
+        }
+        out.flush();
+    }
+
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    private static boolean isNewGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.contentprovider".equals(uri.getAuthority());
+    }
+
+    String getPathFromUri(final Context context, final Uri uri) {
+        String path = getPathFromLocalUri(context, uri);
+        if (path == null) {
+            path = getPathFromRemoteUri(context, uri);
+        }
+        return path;
+    }
+
+    @SuppressLint("NewApi")
+    private String getPathFromLocalUri(final Context context, final Uri uri) {
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            } else if (isDownloadsDocument(uri)) { // https://github.com/flutter/flutter/issues/21863#issuecomment-433064333
+                final String id = DocumentsContract.getDocumentId(uri);
+
+                if (id != null && id.startsWith("raw:")) {
+                    return id.substring(4);
+                }
+
+                String[] contentUriPrefixesToTry = new String[]{
+                        "content://downloads/public_downloads",
+                        "content://downloads/my_downloads",
+                        "content://downloads/all_downloads"
+                };
+
+                for (String contentUriPrefix : contentUriPrefixesToTry) {
+                    Uri contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.valueOf(id));
+                    try {
+                        String path = getDataColumn(context, contentUri, null, null);
+                        if (path != null) {
+                            return path;
+                        }
+                    } catch (Exception e) {
+
+                    }
+                }
+                // path could not be retrieved using ContentResolver, therefore copy file to accessible cache using streams
+                String fileName = getFileName(context, uri);
+                File cacheDir = getDocumentCacheDir(context);
+                File file = generateFileName(fileName, cacheDir);
+                String destinationPath = null;
+                if (file != null) {
+                    destinationPath = file.getAbsolutePath();
+                    saveFileFromUri(context, uri, destinationPath);
+                }
+
+                return destinationPath;
+
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri)) {
+                return uri.getLastPathSegment();
+            } else if (isNewGooglePhotosUri(uri)) {
+
+                // https://github.com/flutter/flutter/issues/21863#issuecomment-433064333
+                String fileName = getFileName(context, uri);
+                File cacheDir = getDocumentCacheDir(context);
+                File file = generateFileName(fileName, cacheDir);
+                String destinationPath = null;
+                if (file != null) {
+                    destinationPath = file.getAbsolutePath();
+                    saveFileFromUri(context, uri, destinationPath);
+                }
+                return destinationPath;
+
+            }
+
+            return getDataColumn(context, uri, null, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
         }
 
-      } else if (isMediaDocument(uri)) {
-        final String docId = DocumentsContract.getDocumentId(uri);
-        final String[] split = docId.split(":");
-        final String type = split[0];
+        return null;
+    }
 
-        Uri contentUri = null;
-        if ("image".equals(type)) {
-          contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-        } else if ("video".equals(type)) {
-          contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-        } else if ("audio".equals(type)) {
-          contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+    private String getFileName(@NonNull Context context, Uri uri) {
+        String mimeType = context.getContentResolver().getType(uri);
+        String filename = null;
+
+        if (mimeType == null) {
+            String path = getPathFromLocalUri(context, uri);
+            if (path == null) {
+                filename = getName(uri.toString());
+            } else {
+                File file = new File(path);
+                filename = file.getName();
+            }
+        } else {
+            Cursor returnCursor = context.getContentResolver().query(uri, null, null, null, null);
+            if (returnCursor != null) {
+                int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                returnCursor.moveToFirst();
+                filename = returnCursor.getString(nameIndex);
+                returnCursor.close();
+            }
         }
 
-        final String selection = "_id=?";
-        final String[] selectionArgs = new String[] {split[1]};
-
-        return getDataColumn(context, contentUri, selection, selectionArgs);
-      }
-    } else if ("content".equalsIgnoreCase(uri.getScheme())) {
-
-      // Return the remote address
-      if (isGooglePhotosUri(uri)) {
-        return uri.getLastPathSegment();
-      }
-
-      return getDataColumn(context, uri, null, null);
-    } else if ("file".equalsIgnoreCase(uri.getScheme())) {
-      return uri.getPath();
+        return filename;
     }
 
-    return null;
-  }
-
-  private static String getDataColumn(
-      Context context, Uri uri, String selection, String[] selectionArgs) {
-    Cursor cursor = null;
-
-    final String column = "_data";
-    final String[] projection = {column};
-
-    try {
-      cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
-      if (cursor != null && cursor.moveToFirst()) {
-        final int column_index = cursor.getColumnIndexOrThrow(column);
-        return cursor.getString(column_index);
-      }
-    } finally {
-      if (cursor != null) {
-        cursor.close();
-      }
+    private void saveFileFromUri(Context context, Uri uri, String destinationPath) {
+        InputStream is = null;
+        BufferedOutputStream bos = null;
+        try {
+            is = context.getContentResolver().openInputStream(uri);
+            bos = new BufferedOutputStream(new FileOutputStream(destinationPath, false));
+            byte[] buf = new byte[1024];
+            is.read(buf);
+            do {
+                bos.write(buf);
+            } while (is.read(buf) != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) is.close();
+                if (bos != null) bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    return null;
-  }
 
-  private static String getPathFromRemoteUri(final Context context, final Uri uri) {
-    // The code below is why Java now has try-with-resources and the Files utility.
-    File file = null;
-    InputStream inputStream = null;
-    OutputStream outputStream = null;
-    boolean success = false;
-    try {
-      inputStream = context.getContentResolver().openInputStream(uri);
-      file = File.createTempFile("image_picker", "jpg", context.getCacheDir());
-      outputStream = new FileOutputStream(file);
-      if (inputStream != null) {
-        copy(inputStream, outputStream);
-        success = true;
-      }
-    } catch (IOException ignored) {
-    } finally {
-      try {
-        if (inputStream != null) inputStream.close();
-      } catch (IOException ignored) {
-      }
-      try {
-        if (outputStream != null) outputStream.close();
-      } catch (IOException ignored) {
-        // If closing the output stream fails, we cannot be sure that the
-        // target file was written in full. Flushing the stream merely moves
-        // the bytes into the OS, not necessarily to the file.
-        success = false;
-      }
+
+    private File getDocumentCacheDir(@NonNull Context context) {
+        File dir = new File(context.getCacheDir(), DOCUMENTS_DIR);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        return dir;
     }
-    return success ? file.getPath() : null;
-  }
 
-  private static void copy(InputStream in, OutputStream out) throws IOException {
-    final byte[] buffer = new byte[4 * 1024];
-    int bytesRead;
-    while ((bytesRead = in.read(buffer)) != -1) {
-      out.write(buffer, 0, bytesRead);
+    @Nullable
+    private File generateFileName(@Nullable String name, File directory) {
+        if (name == null) {
+            return null;
+        }
+
+        File file = new File(directory, name);
+
+        if (file.exists()) {
+            String fileName = name;
+            String extension = "";
+            int dotIndex = name.lastIndexOf('.');
+            if (dotIndex > 0) {
+                fileName = name.substring(0, dotIndex);
+                extension = name.substring(dotIndex);
+            }
+
+            int index = 0;
+
+            while (file.exists()) {
+                index++;
+                name = fileName + '(' + index + ')' + extension;
+                file = new File(directory, name);
+            }
+        }
+
+        try {
+            if (!file.createNewFile()) {
+                return null;
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return file;
     }
-    out.flush();
-  }
 
-  private static boolean isExternalStorageDocument(Uri uri) {
-    return "com.android.externalstorage.documents".equals(uri.getAuthority());
-  }
-
-  private static boolean isDownloadsDocument(Uri uri) {
-    return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-  }
-
-  private static boolean isMediaDocument(Uri uri) {
-    return "com.android.providers.media.documents".equals(uri.getAuthority());
-  }
-
-  private static boolean isGooglePhotosUri(Uri uri) {
-    return "com.google.android.apps.photos.content".equals(uri.getAuthority());
-  }
+    private String getName(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        int index = filename.lastIndexOf('/');
+        return filename.substring(index + 1);
+    }
 }
